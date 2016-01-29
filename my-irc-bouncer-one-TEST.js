d4dddb337eb72cfbc18515e1fe0cfc41300bc797
rahulkondi <kondirahul21@gmail.com>
2016-01-29 15:01:50 +0530

    adds changes to fix most issues at this point

--- my-irc-bouncer-one-TEST.js
+++ my-irc-bouncer-one-TEST.js
@@ -1,8 +1,7 @@
 var BeBot = require('./my-irc-bouncer-one');
 
-BeBot.connect("irc.freenode.org");
-BeBot.join('#birch', 'kondi', 'kondirahul');
+BeBot.connect('irc.freenode.org', 'kondiiiiii', 'kondirahul');
+BeBot.join('#birch');
 BeBot.say('#birch', "Let's Bounce!"); //message into the channel
-BeBot.say('@rahulkondi', "Same Pinch"); //message with nickname as target
-BeBot.leave ('#birch');
-BeBot.disconnect("irc.freenode.org", "Bye bye!");
+//BeBot.leave ('#birch');
+//BeBot.disconnect("irc.freenode.org", "Bye bye!");
--- my-irc-bouncer-one.js
+++ my-irc-bouncer-one.js
@@ -2,43 +2,44 @@
 //it also expects the export methods to be called in a particular order
 
 // Bouncer module
-var irc = require('./IRCBotTheFarm/node_modules/slate-irc');
+var irc = require('slate-irc');
 var net = require('net');
 var client; //a global variable
 
 // connect receivesa a variable that specifies the stream to connect to, in this case 'irc.freenode.org'
-var connect = function (ircStream) {
-    client = net.connect({
+var connect = function (ircStream, nickname, username) {
+  var stream = net.connect({
     port: 6667,
     host: ircStream
   });
-console.log("Connected to " + ircStream);
-}
 
-//join accepts args: channel, nick and username -- in this order. Will add arg for 'realname' later, for now it's hardcoded
-var join = function (channel, nickname, username) {
-  irc.nick(nickname);
+  client = irc(stream);
+  client.nick(nickname);
   client.user(username, 'I iz InternKumar from Scrollback');
-  client.join(channel);
-  console.log("Joined " + channel + " as " + nickname);
+}
+//join accepts args: channel, nick and username -- in this order. Will add arg for 'realname' later, for now it's hardcoded
+var join = function (channel) {
+    if (client.join(channel)) {
+      console.log("Joined " + channel + " as " + nickname);
+    }
 }
 
 //acceppts target as either channel or user with a message
 var say = function (target, message) {
-  client.send(target, message);
-  console.log("Done talkin'");
+  if (client.send(target, message))
+    console.log("Done talkin'");
 }
 
 //to leave a specified channel with a message
 var leave = function (channel, message) {
-  client.part(channel, message);
-  console.log('Left channel ' + channel);
+  if (client.part(channel, message))
+    console.log('Left channel ' + channel);
 }
 
 //disconnect expects the irc stream -- 'irc.freenode.org' for example
 var disconnect = function (ircStream, message) {
-    client.quit(message);
-    console.log("Left network " + ircStream);
+    if (client.quit(message))
+      console.log("Left network " + ircStream);
 }
 
 module.exports = {
